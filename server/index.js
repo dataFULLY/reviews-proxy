b33b1511584ef99f311a0b0ca598888b2e44aba4
aldajani <boss@Bosss-MacBook-Air.local>
2019-09-23 15:04:31 -0700

    Implement proxy-server and set up testing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-console */
+require('newrelic');
 const express = require('express');
+const Axios = require('axios');
+
 const app = express();
 const port = 3000;
 const morgan = require('morgan');
@@ -6,9 +10,43 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 
 app.use(compression());
-app.use('/listings/:listing_id', express.static('./public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/listings/:listings_id', express.static('public'));
+
+app.get('/api/listings/:listing_id/reviews', (req, res) => {
+  const listingID = req.params.listing_id;
+  Axios.get(`http://localhost:3210/api/listings/${listingID}/reviews`)
+    .then((results) => {
+      res.status(200).send(results.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+app.get('/api/listings/:listing_id/host', (req, res) => {
+  const listingID = req.params.listing_id;
+  Axios.get(`http://localhost:3210/api/listings/${listingID}/host`)
+    .then((results) => { res.status(200).send(results.data); })
+    .catch((err) => console.error(err));
+});
+
+app.get('/api/listings/users/:user_id', (req, res) => {
+  const userID = req.params.user_id;
+  Axios.get(`http://localhost:3210/api/listings/users/${userID}`)
+    .then((results) => { res.status(200).send(results.data); })
+    .catch((err) => console.error(err));
+});
+
+app.get('/api/listings/review/response/:response_id', (req, res) => {
+  const responseID = req.params.response_id;
+  Axios.get(`http://localhost:3210/api/listings/review/response/${responseID}`)
+    .then((results) => { res.status(200).send(results.data); })
+    .catch((err) => console.error(err));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
